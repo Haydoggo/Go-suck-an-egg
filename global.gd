c6d8b28fe44d1b9a6a784d0b981133f4b0fb76c0
Charlotte Becconsall <75398987+charlottebecconsall@users.noreply.github.com>
2023-02-26 19:09:42 +1300

    Destroys continuous food
    Also it tracks how much food has been consumed by the vacuum. Consuumed?

---
+++ global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+# GLOBAL VARIABLES WOAAAAH
+var suckitude = {"egg": 0, "flour": 0, "sugar": 0, "cocoa": 0, "vanilla": 0, "unwanted": 0}
--- nozzle.gd
+++ nozzle.gd
@@ -3,13 +3,24 @@ extends Sprite
 
 # Declare member variables
 var nozzle_speed = 200
-var suck_speed = 100
+var discrete_suck_speed = 100
+var continuous_suck_speed = 50
 var consume_speed = 500
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+
+func suckle_from_the_continuous_teat(food, speed, delta):
+	var amount_smaller = (speed*delta)/100
+	food.scale = food.scale.move_toward(Vector2(0, 0), amount_smaller)
+	Global.suckitude[food.type] += amount_smaller
+	food.size -= amount_smaller
+	if food.size <= 0:
+		print("trying to destroy continuous food")
+		food.queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,21 +30,18 @@ func _process(delta):
 	for area in get_node("Suck").get_overlapping_areas():
 		var food : Node2D = area.owner
 		if food.is_in_group("discrete"):
-			food.position = food.position.move_toward(position, suck_speed*delta)
+			food.position = food.position.move_toward(position, discrete_suck_speed*delta)
 		else:
-			food.scale = food.scale.move_toward(Vector2(0, 0), (suck_speed*delta)/100)
-			if food.scale == Vector2(0, 0):
-				food.queue_free()
-			
+			suckle_from_the_continuous_teat(food, continuous_suck_speed, delta)
+	
 	for area in get_node("Consume").get_overlapping_areas():
 		var food : Node2D = area.owner
 		if food.is_in_group("discrete"):
 			food.position = food.position.move_toward(position, consume_speed*delta)
 			if food.position == position:
 				food.queue_free()
+				Global.suckitude[food.type] += 1
 		else:
-			food.scale = food.scale.move_toward(position, (consume_speed*delta)/100)
-			if food.scale == Vector2(0, 0):
-				food.queue_free()
+			suckle_from_the_continuous_teat(food, continuous_suck_speed, delta)
 			
 		
