d7e16990f8ad77e2f18996b93001bc8d313158cc
Charlotte Becconsall <75398987+charlottebecconsall@users.noreply.github.com>
2023-02-26 18:10:25 +1300

    Sucking up them eggs

--- nozzle.gd
+++ nozzle.gd
@@ -3,7 +3,8 @@ extends Sprite
 
 # Declare member variables
 var nozzle_speed = 200
-# var b = "text"
+var suck_speed = 100
+var consume_speed = 500
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,4 +15,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position = position.move_toward(get_global_mouse_position(), nozzle_speed*delta)
-	pass
+	for area in get_node("Suck").get_overlapping_areas():
+		var food : Node2D = area.owner
+		if food.is_in_group("discrete"):
+			food.position = food.position.move_toward(position, suck_speed*delta)
+		else:
+			# make the size smaller at suck speed
+			food.scale = food.scale.move_toward(Vector2(0, 0), (suck_speed*delta)/100)
+	for area in get_node("Consume").get_overlapping_areas():
+		var food = area.owner
+		food.position = food.position.move_toward(position, consume_speed*delta)
+		if food.position == position:
+			food.queue_free()
